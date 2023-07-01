d7bf93e8e8a8c45ee4ddc5df843be8ad9ba8038c
Waleed Alhindi <wl392785@dal.ca>
2023-07-01 09:44:08 -0300

    Finalized A2 Implementation

--- A2/container1/src/index.js
+++ A2/container1/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './server.js';
+import App from './Registration.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- A2/container3/src/index.js
+++ A2/container3/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Server.js';
+import App from './LandingPage.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
